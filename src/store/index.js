e0df0b7986d3164e33e339bb03c82250c41fa4ae
cchura94 <cchura.cpc@gmail.com>
2022-06-07 18:42:30 -0400

    Proyecto base

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,27 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    contador: 0,
+    usuario: null
   },
   getters: {
   },
   mutations: {
+    aumentar(state) {
+      state.contador++;
+    },
+    login(state, datos_usuario){
+      state.usuario = datos_usuario
+    }
   },
   actions: {
+    incrementarContador(context){
+      context.commit("aumentar")
+    },
+    ingresarVuex(context, datos_usuario){
+      context.commit("login", datos_usuario);
+
+    }
   },
   modules: {
   }
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,40 @@
+<template>
+    <h1>Ingresar</h1>
+    <h2>{{ $store.state.contador }}</h2>
+    <button @click="incrementar()">+</button>
+
+    <br>
+
+    <label for="">Ingrese Correo</label>
+    <input type="email" v-model="usuario.email">
+    <br>
+    <label for="">Ingrese Contraseña</label>
+    <input type="password" v-model="usuario.password">
+    <br>
+    <button @click="ingresar()">Ingresar</button>
+
+    <h1>{{ $store.state.usuario }}</h1>
+  
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            usuario: {
+                email: '',
+                password: ''
+            }
+        }
+    },
+    methods: {
+        incrementar(){
+            this.$store.dispatch('incrementarContador')
+        },
+        ingresar(){
+            this.$store.dispatch('ingresarVuex', this.usuario)
+        }
+    }
+
+}
+</script>
